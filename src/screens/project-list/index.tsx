c7ac0102b467f450ea4a300e4c75014f36fe6dd5
sbill <1422347728@qq.com>
2022-02-15 22:28:30 +0800

    优化项目列表页面

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -4,6 +4,7 @@ import { List } from "./list"
 import { SearchPanel } from "./search-panel"
 import * as qs from 'qs'
 import { useHttp } from 'utils/https'
+import styled from '@emotion/styled'
 
 const apiUrl = process.env.REACT_APP_API_URL
 
@@ -25,8 +26,13 @@ export const ProjectListScreen = () => {
     client('users').then(setUsers)
   })
 
-  return <div>
+  return <Container>
+    <h1>项目列表</h1>
     <SearchPanel users={users} param={param} setParam={setParam} />
     <List users={users} list={list} />
-  </div>
+  </Container>
 }
+
+const Container = styled.div`
+  padding: 3.2rem;
+`
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,4 +1,4 @@
-import { Input, Select } from 'antd';
+import { Form, Input, Select } from 'antd';
 
 export interface User {
   id: string;
@@ -20,12 +20,19 @@ interface SearchPanelProps {
 
 export const SearchPanel = ({users, param, setParam}: SearchPanelProps) => {
   
-  return <form>
-    <div>
-      <Input type="text" value={param.name} onChange={evt => setParam({
-        ...param,
-        name: evt.target.value
-      })} />
+  return <Form style={{marginBottom: '2rem'}} layout='inline'>
+    <Form.Item>
+      <Input
+        placeholder='项目名'
+        type="text"
+        value={param.name}
+        onChange={evt => setParam({
+          ...param,
+          name: evt.target.value
+        })}
+      />
+    </Form.Item>
+    <Form.Item>
       <Select value={param.personId} onChange={value => setParam({
         ...param,
         personId: value
@@ -35,6 +42,6 @@ export const SearchPanel = ({users, param, setParam}: SearchPanelProps) => {
           users.map(user => <Select.Option key={user.id} value={user.id}>{user.name}</Select.Option>)
         }
       </Select>
-    </div>
-  </form>
+    </Form.Item>
+  </Form>
 }
