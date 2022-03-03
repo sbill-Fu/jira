7fb0f3f3b296f9864a81f0b4034b2244975a9abe
sbill <1422347728@qq.com>
2022-03-03 22:06:10 +0800

    feat: kanban 和 epic 路由

--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,3 +1,18 @@
+import { Link } from 'react-router-dom'
+import {Routes, Route, Navigate } from 'react-router'
+import { EpicScreen } from 'screens/epic'
+import { KanbanScreen } from 'screens/kanban'
+import { ProjectListScreen } from 'screens/project-list'
+
 export const ProjectScreen = () => {
-  return <h1>project screen</h1>
+  return <div>
+    <h1>ProjectScreen</h1>
+    <Link to='kanban'>看板</Link>
+    <Link to='epic'>任务组</Link>
+    <Routes>
+      <Route path='/kanban' element={<KanbanScreen />} />
+      <Route path='/epic' element={<EpicScreen />} />
+      <Route path='*' element={<KanbanScreen />} />
+    </Routes>
+  </div>
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -60,3 +60,5 @@ export const useDocumentTitle = (title: string, keepOnUnmount: boolean = true) =
     }
   }, [keepOnUnmount, oldTitle])
 }
+
+export const resetRoute = () => window.location.href = window.location.origin
