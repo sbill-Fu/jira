ca370a68b3fde1d14a093ebcd4e580ca1dc28ef7
sbill <1422347728@qq.com>
2022-03-20 14:21:36 +0800

    feat: 删除和乐观更新

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,11 +1,11 @@
-import { Dropdown, Menu, Table, TableProps } from 'antd';
+import { Dropdown, Menu, Modal, Table, TableProps } from 'antd';
 import { ButtonNoPadding } from 'components/lib';
 import { Pin } from 'components/pin';
 import dayjs from 'dayjs';
 import { Link } from 'react-router-dom';
-import { useEditProject } from 'utils/project';
+import { useDeleteProject, useEditProject } from 'utils/project';
 import { User } from './search-panel';
-import { useProjectModal } from './utils';
+import { useProjectModal, useProjectsQueryKey } from './utils';
 
 export interface Project {
   id: number;
@@ -21,10 +21,8 @@ interface ListProps extends TableProps<Project>{
 }
 
 export const List = ({users, loading, ...props}: ListProps) => {
-  const { mutate } = useEditProject()
-  const { startEdit } = useProjectModal()
+  const { mutate } = useEditProject(useProjectsQueryKey())
   const pinProject = (id: number) => (pin: boolean) => mutate({id, pin})
-  const editProject = (id: number) => () => startEdit(id)
 
   return <Table
     loading={loading}
@@ -66,15 +64,33 @@ export const List = ({users, loading, ...props}: ListProps) => {
       {
         title: '操作',
         render(value, project) {
-          return <Dropdown overlay={<Menu>
-            <Menu.Item onClick={editProject(project.id)} key={'edit'} >编辑</Menu.Item>
-            <Menu.Item key={'delete'}>删除</Menu.Item>
-          </Menu>}>
-            <ButtonNoPadding type='link'>...</ButtonNoPadding>
-          </Dropdown>
+          return <More project={project} />
         }
       }
     ]}
     {...props}
     />
+}
+
+const More = ({project}: {project: Project}) => {
+  const { startEdit } = useProjectModal()
+  const editProject = (id: number) => () => startEdit(id)
+  const {mutate: deleteProject} = useDeleteProject(useProjectsQueryKey())
+  const confirmDeleteProject = (id: number) => {
+    Modal.confirm({
+      title: '确定删除这个项目吗？',
+      content: '点击确定删除',
+      okText: '确定',
+      onOk() {
+        deleteProject({id})
+      }
+    })
+  }
+
+  return <Dropdown overlay={<Menu>
+    <Menu.Item onClick={editProject(project.id)} key={'edit'} >编辑</Menu.Item>
+    <Menu.Item key={'delete'} onClick={() => confirmDeleteProject(project.id)}>删除</Menu.Item>
+  </Menu>}>
+    <ButtonNoPadding type='link'>...</ButtonNoPadding>
+  </Dropdown>
 }
--- src/utils/project.ts
+++ src/utils/project.ts
@@ -1,9 +1,11 @@
 import { useCallback, useEffect } from 'react'
-import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { QueryKey, useMutation, useQuery, useQueryClient } from 'react-query'
 import { Project } from 'screens/project-list/list'
 import { cleanObject } from 'utils'
 import { useHttp } from './https'
 import { useAsync } from './use-async'
+import { useProjectsSearchParams } from '../screens/project-list/utils'
+import { useAddConfig, useDeleteConfig, useEditConfig } from './use-optimistic-options'
 
 export const useProjects = (param?: Partial<Project>) => {
   const client = useHttp()
@@ -11,33 +13,38 @@ export const useProjects = (param?: Partial<Project>) => {
   return useQuery<Project[]>(['projects', param], () => client('projects', {data: param}))
 }
 
-export const useEditProject = () => {
+export const useEditProject = (queryKey: QueryKey) => { 
   const client = useHttp()
-  const queryClient = useQueryClient()
   
   return useMutation(
     (params: Partial<Project>) => client(`projects/${params.id}`, {
       method: 'PATCH',
       data: params
     }), 
-    {
-      onSuccess: () => queryClient.invalidateQueries('projects')
-    }
+    useEditConfig(queryKey)
   )
 }
 
-export const useAddProject = () => {
+export const useAddProject = (queryKey: QueryKey) => {
   const client = useHttp()
-  const queryClient = useQueryClient()
 
   return useMutation((params: Partial<Project>) => 
     client(`projects`, {
       data: params,
       method: 'POST'
     }),
-    {
-      onSuccess: () => queryClient.invalidateQueries('projects')
-    }
+    useAddConfig(queryKey)
+  )
+}
+
+export const useDeleteProject = (queryKey: QueryKey) => {
+  const client = useHttp()
+
+  return useMutation(({id}: {id: number}) => 
+    client(`projects/${id}`, {
+      method: 'DELETE'
+    }),
+    useDeleteConfig(queryKey)
   )
 }
 
