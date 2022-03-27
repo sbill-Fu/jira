937bc97ec36c66d32aa38e0c7729421491184481
sbill <1422347728@qq.com>
2022-03-27 22:18:33 +0800

    feat: 添加 search tasks 功能

--- src/screens/kanban/index.tsx
+++ src/screens/kanban/index.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled'
 import { useDocumentTitle } from 'utils'
 import { useKanbans } from 'utils/kanban'
 import { KanbanColumn } from './kanban-column'
+import { SearchPanel } from './search-panel'
 import { useKanbanSearchParams, useProjectInUrl } from './util'
 
 export const KanbanScreen = () => {
@@ -11,6 +12,7 @@ export const KanbanScreen = () => {
   const {data: kanbans} = useKanbans(useKanbanSearchParams())
   return <div>
     <h1>{currentProject?.name}看板</h1>
+    <SearchPanel />
     <ColumnsContainer>
       {
         kanbans?.map(kanban => <KanbanColumn kanban={kanban} key={kanban.id} />)
