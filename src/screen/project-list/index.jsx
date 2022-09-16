7bdb6bb63c1c9f2541564b5becd48672cba19d88
sbill <1422347728@qq.com>
2022-09-16 21:56:50 +0800

    3-4 学习自定义Hook - 用useDebounce减少工程搜索请求频率

--- src/screen/project-list/index.jsx
+++ src/screen/project-list/index.jsx
@@ -1,37 +1,41 @@
-import { List } from "./list"
-import { SearchPanel } from "./search-panel"
-import {useState, useEffect} from 'react'
-import { cleanObject } from "utils"
+import { List } from './list'
+import { SearchPanel } from './search-panel'
+import { useState, useEffect } from 'react'
+import { cleanObject, useDebounce, useMount } from 'utils'
 import * as qs from 'qs'
 
 const apiUrl = process.env.REACT_APP_API_URL
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: '',
-    personId: ''
+    personId: '',
   })
   const [list, setList] = useState([])
   const [users, setUsers] = useState([])
-
+  const debounceParam = useDebounce(param, 2000)
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`
+    ).then(async (response) => {
       if (response.ok) {
         setList(await response.json())
       }
     })
-  }, [param])
+  }, [debounceParam])
 
-  useEffect(() => {
-    fetch(`${apiUrl}/users`).then(async response => {
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
 
-  return <div>
-    <SearchPanel param={param} setParam={setParam} users={users} />
-    <List list={list} users={users} />
-  </div>
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List list={list} users={users} />
+    </div>
+  )
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 export const isFalsy = value => value === 0 ? false : !value
 
 /**
@@ -11,4 +13,19 @@ export const cleanObject = object => {
     if (isFalsy(value)) Reflect.deleteProperty(result, key)
   })
   return result
+}
+
+export const useMount = callback => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value)
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebounceValue(value), delay)
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+  return debounceValue
 }
