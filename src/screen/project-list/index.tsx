3f6dd3af27e9f5fb949af47f4f47273847ee9afd
sbill <1422347728@qq.com>
2022-09-18 11:26:56 +0800

    5-8 用useHttp管理JWT和登录状态，保持登录状态

--- src/screen/project-list/index.tsx
+++ src/screen/project-list/index.tsx
@@ -3,8 +3,8 @@ import { SearchPanel } from './search-panel'
 import { useState, useEffect } from 'react'
 import { cleanObject, useDebounce, useMount } from 'utils'
 import * as qs from 'qs'
+import { useHttp } from 'utils/http'
 
-const apiUrl = process.env.REACT_APP_API_URL
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: '',
@@ -13,23 +13,14 @@ export const ProjectListScreen = () => {
   const [list, setList] = useState([])
   const [users, setUsers] = useState([])
   const debounceParam = useDebounce(param, 200)
+  const client = useHttp()
 
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json())
-      }
-    })
+    client('projects', cleanObject(debounceParam)).then(setList)
   }, [debounceParam])
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json())
-      }
-    })
+    client('users').then(setUsers)
   })
 
   return (
