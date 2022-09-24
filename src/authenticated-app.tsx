eb1f11b3f1242b2ab4d73eed3fb8b18114e7c0a9
sbill <1422347728@qq.com>
2022-09-24 16:57:16 +0800

    6-6 用CSS-in-JS状态工程创建自定义组件：Row组件实现

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,15 +1,16 @@
 import styled from '@emotion/styled'
+import { Row } from 'components/lib'
 import { useAuth } from 'context/auth-context'
 import { ProjectListScreen } from 'screen/project-list'
 
 export const AuthenticatedApp = () => {
   const {logout} = useAuth()
   return <Container>
-    <Header>
-      <HeaderLeft>
-        <h3>logo</h3>
-        <h3>项目</h3>
-        <h3>用户</h3>
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <h2>logo</h2>
+        <h2>项目</h2>
+        <h2>用户</h2>
       </HeaderLeft>
       <HeaderRight>
         <button onClick={logout}>登出</button>
@@ -21,22 +22,17 @@ export const AuthenticatedApp = () => {
   </Container>
 }
 
+
 const Container = styled.div`
   display: grid;
   grid-template-rows: 6rem calc(100vh - 6rem);
   height: 100vh;
 `
 
-const Header = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+const Header = styled(Row)`
 `
 
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
+const HeaderLeft = styled(Row)`
 `
 
 const HeaderRight = styled.div`
