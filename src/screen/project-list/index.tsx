e52016c8836eeb13f9149b913cfbaaf69dbda420
sbill <1422347728@qq.com>
2022-09-25 15:04:25 +0800

    6-7 完善项目列表页面样式

--- src/screen/project-list/index.tsx
+++ src/screen/project-list/index.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import { cleanObject, useDebounce, useMount } from 'utils'
 import * as qs from 'qs'
 import { useHttp } from 'utils/http'
+import styled from '@emotion/styled'
 
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
@@ -24,9 +25,14 @@ export const ProjectListScreen = () => {
   })
 
   return (
-    <div>
+    <Container>
+      <h1>项目列表</h1>
       <SearchPanel param={param} setParam={setParam} users={users} />
       <List list={list} users={users} />
-    </div>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  padding: 3.2rem;
+`
