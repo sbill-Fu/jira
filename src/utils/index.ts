abc63d19e18501de91a0507169d9ec216986eda1
sbill <1422347728@qq.com>
2022-09-29 15:19:59 +0800

    8-2 ⽤useRef实现useDocumentTitle - useRef与Hook 闭包详解（下）

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export const isFalsy = (value: unknown) => value === 0 ? false : !value
 
@@ -34,8 +34,7 @@ export const useDebounce = <V>(value: V, delay?: number) => {
 }
 
 export const useDocumentTitle = (title: string, keepOnUnmount: boolean = true) => {
-  const oldTitle = document.title
-  
+  const oldTitle = useRef(document.title).current
   useEffect(() => {
     document.title = title
   }, [title])
@@ -46,5 +45,5 @@ export const useDocumentTitle = (title: string, keepOnUnmount: boolean = true) =
         document.title = oldTitle
       }
     }
-  }, [])
+  }, [keepOnUnmount, oldTitle])
 }
