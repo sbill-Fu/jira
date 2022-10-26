7336ea310dcd3e8eb195c44c7facd11fc1bc95b5
sbill <1422347728@qq.com>
2022-10-26 09:47:04 +0800

    8-3 添加项目列表和项目详情路由

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -4,34 +4,47 @@ import { useAuth } from 'context/auth-context'
 import { ProjectListScreen } from 'screen/project-list'
 import { ReactComponent as SoftwareLogo} from 'assets/software-logo.svg'
 import { Button, Dropdown, Menu } from 'antd'
+import { Navigate, Route, Routes } from 'react-router'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { ProjectScreen } from 'screen/project'
 
 export const AuthenticatedApp = () => {
-  const {logout, user} = useAuth()
   return <Container>
-    <Header between={true}>
-      <HeaderLeft gap={true}>
-        <SoftwareLogo width={'18rem'} color='rgb(38, 132, 255)' />
-        <h2>项目</h2>
-        <h2>用户</h2>
-      </HeaderLeft>
-      <HeaderRight>
-        <Dropdown overlay={<Menu>
-          <Menu.Item>
-            <Button type='link' onClick={logout}>登出</Button>
-          </Menu.Item>
-        </Menu>}>
-          <Button type='link'>
-            Hi, {user?.name}
-          </Button>
-        </Dropdown>
-      </HeaderRight>
-    </Header>
+    <PageHeader />
     <main>
-      <ProjectListScreen />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Navigate to='projects' />} />
+          {/* <Route index element={<ProjectListScreen />} /> */}
+          <Route path='/projects' element={<ProjectListScreen />} />
+          <Route path='/projects/:projectId/*' element={<ProjectScreen />} />
+        </Routes>
+      </Router>
     </main>
   </Container>
 }
 
+const PageHeader = () => {
+  const {logout, user} = useAuth()
+  return <Header between={true}>
+    <HeaderLeft gap={true}>
+      <SoftwareLogo width={'18rem'} color='rgb(38, 132, 255)' />
+      <h2>项目</h2>
+      <h2>用户</h2>
+    </HeaderLeft>
+    <HeaderRight>
+      <Dropdown overlay={<Menu>
+        <Menu.Item>
+          <Button type='link' onClick={logout}>登出</Button>
+        </Menu.Item>
+      </Menu>}>
+        <Button type='link'>
+          Hi, {user?.name}
+        </Button>
+      </Dropdown>
+    </HeaderRight>
+  </Header>
+}
 
 const Container = styled.div`
   display: grid;
--- src/screen/project-list/list.tsx
+++ src/screen/project-list/list.tsx
@@ -1,6 +1,7 @@
 import { Table, TableProps } from 'antd'
 import { User } from 'screen/project-list/search-panel'
 import dayjs from 'dayjs'
+import { Link } from 'react-router-dom'
 
 export interface Project {
   id: string;
@@ -16,21 +17,43 @@ interface ListProps extends TableProps<Project> {
 }
 
 export const List = ({users, ...props}: ListProps) => {
-  return <Table pagination={false} columns={[
-    { title: '名称', dataIndex: 'name', sorter: (a, b) => a.name.localeCompare(b.name) },
-    { title: '部门', dataIndex: 'organization' },
-    { title: '负责人', render(value, project) {
-      return <span>
-        {users.find(user => user.id === project.personId)?.name || '未知'}
-      </span>
-    }},
-    {
-      title: '创建时间',
-      render(value, project) {
-        return <span>
-          { project.created ? dayjs(project.created).format('YYYY-MM-DD') : '-' }
-        </span>
-      }
-    }
-  ]} {...props} />
+  return (
+    <Table
+      pagination={false}
+      columns={[
+        {
+          title: '名称',
+          sorter: (a, b) => a.name.localeCompare(b.name),
+          render(value, project) {
+            return <Link to={String(project.id)}>{project.name}</Link>
+          }
+        },
+        { title: '部门', dataIndex: 'organization' },
+        {
+          title: '负责人',
+          render(value, project) {
+            return (
+              <span>
+                {users.find((user) => user.id === project.personId)?.name ||
+                  '未知'}
+              </span>
+            )
+          }
+        },
+        {
+          title: '创建时间',
+          render(value, project) {
+            return (
+              <span>
+                {project.created
+                  ? dayjs(project.created).format('YYYY-MM-DD')
+                  : '-'}
+              </span>
+            )
+          }
+        }
+      ]}
+      {...props}
+    />
+  )
 }
